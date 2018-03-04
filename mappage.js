b586ec8bbf94d98168bbbf5a5e150ee02b4e8baa
shian27 <36091775+shian27@users.noreply.github.com>
2018-03-04 14:33:53 -0500

    Update mappage.js

--- mappage.js
+++ mappage.js
@@ -1,38 +1,24 @@
-//mappage.js
-
-
-var app = require('express')();
+var express = require('express');
+//var app = require('express')();
+var path = require('path');
+var app = express();
 var http = require('http').Server(app);
 var hbs = require('hbs');
 var fs = require('fs');
 //var d = new Date(1-12-2018);
 
+var publicPath = path.resolve(__dirname, 'public');
 
+app.use(express.static(publicPath));
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 
 //Original Message
-
 app.get('/', function(req, res) {
-	var searches = JSON.parse(
-		fs.readFileSync('mappage.json', 'utf8')
-		);
 		res.render(__dirname + '/mappage.html', {
 		'welcomeMessage': 'Find Free Food Here'
 	});
 });
-
-//Return Message from JSON
-
-app.get('/message', function(req, res) {
-	console.log(req.query);
-	var searches = JSON.parse(
-		fs.readFileSync('mappage.json', 'utf8'));
-	searches.push(req.query);
-	fs.writeFileSync('mappage.json', JSON.stringify(searches));
-	res.redirect('/');
-})
-
 http.listen(3000, function() {
 	console.log("listening on port 3000");
 })
